d5e40f0fdbcdb1108f980704a04d6eb955f0d5d7
wkn816 <wkn816@gmail.com>
2024-09-20 00:41:38 +0900

    fix_app.vuekara

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,12 @@
     <MyText />
     <MyTitle />
     <MyForm @submit-form="getWeather" />
-    <MyResults />
+    <MyResults :results="results" />
   </div>
 </template>
 
 <script setup>
+import { reactive } from "vue";
 import axios from "axios";
 import HelloWorld from "./components/HelloWorld.vue";
 import MyText from "./components/MyText.vue";
@@ -17,13 +18,25 @@ import MyTitle from "./components/MyTitle.vue";
 import MyForm from "./components/MyForm.vue";
 import MyResults from "./components/MyResults.vue";
 
-
+const results = reactive({
+  country: "",
+  cityName: "",
+  temperature: "",
+  conditionText: "",
+  icon: ""
+})
 const getWeather = () => {
   axios
     .get(
       "https://api.weatherapi.com/v1/current.json?key=d93185177c9a41d4bb9111921241909&q=London&aqi=no"
     )
-    .then(res => console.log(res))
+    .then(res => {
+            results.country = res.data.location.country,
+            results.cityName = res.data.location.name,
+            results.temperature = res.data.current.temp_c,
+            results.conditionText = res.data.current.condition.text,
+            results.icon = res.data.current.condition.icon
+    })
 }
 </script>
 
--- src/components/MyResults.vue
+++ src/components/MyResults.vue
@@ -1,6 +1,14 @@
 <template>
-  <h2>気象データ</h2>
+  <div>
+    <h2>気象データ</h2>
+    {{ results }}
+  </div>
 </template>
-<script>
+<script setup>
+import { defineProps } from "vue";
 
+const props = defineProps({
+  results: Object
+});
+console.log(props) //ここ消すとpropsが一度も使われてないよ。と怒られるので、とりあえず吐き出す
 </script>
