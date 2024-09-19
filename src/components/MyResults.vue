b5f4a5c2329594d1f66afedaa95c429dc8f64681
wkn816 <wkn816@gmail.com>
2024-09-20 00:53:28 +0900

    fix_v-ifを追加して値が入っている時だけdivタグを表示

--- src/components/MyResults.vue
+++ src/components/MyResults.vue
@@ -1,7 +1,13 @@
 <template>
   <div>
     <h2>気象データ</h2>
-    {{ results }}
+  <div v-if="results.country">{{ results.country }}</div>
+  <div v-if="results.cityName">{{ results.cityName }}</div>
+  <div v-if="results.temperature">{{ results.temperatur }}</div>
+  <div v-if="results.icon">
+    <img :src="results.icon" alt="icon">
+    <span>{{ results.conditionText }}</span>
+  </div>
   </div>
 </template>
 <script setup>
