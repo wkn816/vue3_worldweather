3f51e2effbb9a62dbae8a4de8a2440b72f481bb1
wkn816 <wkn816@gmail.com>
2024-09-23 16:10:43 +0900

    “メッセージ”

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,12 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="" />
-    <MyText />
-    <MyTitle />
-    <MyForm @submit-form="getWeather" />
-    <MyResults :results="results" />
-  </div>
+  <v-app>
+    <v-main>
+      <router-view/>
+      <MyTitle />
+    </v-main>
+  </v-app>
 </template>
 
 <script setup>
-import { reactive } from "vue";
-import axios from "axios";
-import HelloWorld from "./components/HelloWorld.vue";
-import MyText from "./components/MyText.vue";
-import MyTitle from "./components/MyTitle.vue";
-import MyForm from "./components/MyForm.vue";
-import MyResults from "./components/MyResults.vue";
-
-const results = reactive({
-  country: "",
-  cityName: "",
-  temperature: "",
-  conditionText: "",
-  icon: ""
-})
-const getWeather = (city) => {
-  axios
-    .get(
-      `https://api.weatherapi.com/v1/current.json?key=d93185177c9a41d4bb9111921241909&q=${city}&aqi=no`
-    )
-    .then(res => {
-            results.country = res.data.location.country,
-            results.cityName = res.data.location.name,
-            results.temperature = res.data.current.temp_c,
-            results.conditionText = res.data.current.condition.text,
-            results.icon = res.data.current.condition.icon
-    })
-}
+import MyTitle from "./components/MyTitle.vue"
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,18 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
+import vuetify from './plugins/vuetify'
+import { loadFonts } from './plugins/webfontloader'
+import VueScrollTo from 'vue-scrollto'
 
-createApp(App).mount('#app')
+
+loadFonts()
+
+createApp(App)
+  .use(router)
+  .use(vuetify)
+  .mount('#app')
+  .use(VueScrollTo, {
+    duration: 500,  // スクロールのスピード（ミリ秒）
+    easing: "ease-in-out",  // スクロールアニメーションの種類
+  });
